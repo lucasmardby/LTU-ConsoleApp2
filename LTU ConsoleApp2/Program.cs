ce57f5e34f42a0824228e3a7d887c8bd64f49819
lucasmardby <lucasmardby@gmail.com>
2024-03-14 09:37:40 +0100

    First commit

---
+++ LTU ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LTU_ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
